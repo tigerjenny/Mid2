745e5a4bfa49ae211e173a09ada2611b6a25f781
Jenny <a0989783233@gmail.com>
2017-12-08 17:14:39 +0800

    homework 加入圖片

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -1,31 +1,40 @@
 package com.fju.mid2;
 
+import android.media.Image;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ImageView;
 
 import java.util.Random;
 
 public class Func1Activity extends AppCompatActivity {
 
-    private TextView txNum;
+    int[] dies = {R.drawable.d1,
+                    R.drawable.d2,
+                    R.drawable.d3,
+                    R.drawable.d4,
+                    R.drawable.d5,
+                    R.drawable.d6};
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_func1);
+        imageView = (ImageView) findViewById(R.id.imageView);
     }
 
     public void roll(View view){
         int i = new Random().nextInt(6)+1;
-        new AlertDialog.Builder(this)
-                .setTitle("Message")
-                .setMessage("Number: "+i)
-                .setPositiveButton("OK" , null)
-                .show();
+//        new AlertDialog.Builder(this)
+//                .setTitle("Message")
+//                .setMessage("Number: "+i)
+//                .setPositiveButton("OK" , null)
+//                .show();
+        imageView.setImageResource(dies[i-1]);
+
     }
 
 }
